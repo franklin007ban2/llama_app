90341f6df9da790fb9589b857e4f504a33f65c6c
sudo888samewick <150644414+sudo888samewick@users.noreply.github.com>
2025-01-12 03:54:07 +0000

    .

--- zhipu.ts
+++ zhipu.ts
@@ -1,24 +1,10 @@
-import { ZhipuAI } from 'zhipuai-sdk-nodejs-v4';
+process.on('beforeExit', (code) => {
+    console.log(`Process is about to exit with code: ${code}`);
+  });
 
-const createEmbeddings = async () => {
-    const ai = new ZhipuAI({
-        apiKey: '9fcb4a484e274be7baecc084a3ee9417.y6feosES220aOook',
-        baseUrl: 'https://open.bigmodel.cn/api/paas/v4'
-    })
-    const result = await ai.createEmbeddings({
-        model: "embedding-3",
-        input: "你好，请问你是谁",
-        encodingFormat: '',
-        user: '',
-        sensitiveWordCheck: undefined as any
-    })
-    console.log(result.data, "embedding")
+const func = async function () {
+    const result = await Promise.resolve(42);
+    console.log(result); // 输出: 42
 }
+func()
 
-// 调用 async 函数
-const main = async () => {
-    await createEmbeddings();
-}
-
-// 确保 main 执行完毕
-main();
